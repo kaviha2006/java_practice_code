a84ae022fbefce7915966ed400a1af6dc68866da
Kaviha R. M <kaviharavichandran2006@gmail.com>
2025-09-05 16:47:51 +0530

    Add files via upload

---
+++ Pattern Problems/left_pascal_triangle.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class left_pascal_triangle {
+    public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        int n=src.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+}
+/*output:
+5
+    *
+   **
+  ***
+ ****
+*****
+ ****
+  ***
+   **
+    *
+ */

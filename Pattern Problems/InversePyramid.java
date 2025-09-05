4ce048f65d5ee42df253012572555d7d3e1ce1ad
Kaviha R. M <kaviharavichandran2006@gmail.com>
2025-09-05 18:08:07 +0530

    Add files via upload

---
+++ Pattern Problems/InversePyramid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class InversePyramid {
+    public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        int n=src.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*output:
+5
+*********
+ *******
+  *****
+   ***
+    *
+ */

15c89190fd134aae221ef50eb9ff285ca4532ebb
Kaviha R. M <kaviharavichandran2006@gmail.com>
2025-09-05 18:40:41 +0530

    Add files via upload

---
+++ Pattern Problems/inverse_right_triangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class inverse_right_triangle {
+     public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        int n=src.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*output:
+5
+*****
+ ****
+  ***
+   **
+    *
+ */

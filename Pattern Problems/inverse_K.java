382290c3eae2aa91b4d5e74fad55eaea65abcf9a
Kaviha R. M <kaviharavichandran2006@gmail.com>
2025-09-05 19:46:58 +0530

    Add files via upload

---
+++ Pattern Problems/inverse_K.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class inverse_K {
+    public static void main(String[] args) {
+        Scanner scr=new Scanner(System.in);
+        int n=scr.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*output:
+5
+*****
+ ****
+  ***
+   **
+    *
+   **
+  ***
+ ****
+*****
+ */

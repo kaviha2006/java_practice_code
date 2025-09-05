4bc09baae288245c1750d92bbb12a84c6a8b138d
Kaviha R. M <kaviharavichandran2006@gmail.com>
2025-09-05 18:29:37 +0530

    Add files via upload

---
+++ Pattern Problems/hollowpyramid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class hollowpyramid {
+    public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        int n=src.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                if(i==n||j==1||j==2*i-1)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*output:
+5
+    *
+   * *
+  *   *
+ *     *
+*********
+ */
